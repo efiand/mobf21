fa64cc35eeb6621d47e756c61eecc88ad603f4ef
Андрей Ефимов <efiand@ya.ru>
2020-08-30 21:23:32 +0300

    const

--- gulpfile.js/const.js
+++ gulpfile.js/const.js
@@ -1,27 +1,12 @@
-const svgoPrecision = {
-	floatPrecision: 2
-};
-
 module.exports = {
-	copySource: [
+	COPY_SOURCE: [
 		`source/as-is/**/.*`,
 		`source/as-is/**/*.*`
 	],
-	dist: `public`,
-	eslintSource: [
+	DIST: `public`,
+	ESLINT_SOURCE: [
 		`gulpfile.js/**/*.js`,
 		`source/data/**/*.js`,
 		`source/js/**/*.js`
-	],
-	svgoConfig: {
-		plugins: [
-			{ removeViewBox: false },
-			{ removeTitle: true },
-			{ cleanupNumericValues: svgoPrecision },
-			{ convertPathData: svgoPrecision },
-			{ transformsWithOnePath: svgoPrecision },
-			{ convertTransform: svgoPrecision },
-			{ cleanupListOfValues: svgoPrecision }
-		]
-	}
+	]
 };
--- gulpfile.js/tasks/server.js
+++ gulpfile.js/tasks/server.js
@@ -2,7 +2,7 @@ const { series, watch } = require(`gulp`);
 const tasks = require(`require-dir`)(`.`);
 const { twighint, stylelint, eslint, copy, html, css } = tasks;
 const browserSync = require(`browser-sync`).create();
-const { copySource, dist, eslintSource } = require(`../const`);
+const { COPY_SOURCE, DIST, ESLINT_SOURCE } = require(`../const`);
 
 const opts = {
 	cors: true,
@@ -18,7 +18,7 @@ if (siteArg) {
 	// Пример запуска с проксированием домена (Open Server etc.): `yarn start -- --mylocaldomain.dev`
 	opts.proxy = `https://${siteArg.slice(ARG_DASHES_COUNT)}`;
 } else {
-	opts.server = dist;
+	opts.server = DIST;
 }
 
 const reload = (done) => {
@@ -31,8 +31,8 @@ const server = () => {
 
 	watch(`source/**/*.twig`, series(twighint, html, reload));
 	watch(`source/less/**/*.less`, series(stylelint, css, reload));
-	watch(eslintSource, series(eslint));
-	watch(copySource, series(copy, reload));
+	watch(ESLINT_SOURCE, series(eslint));
+	watch(COPY_SOURCE, series(copy, reload));
 };
 
 module.exports = server;
